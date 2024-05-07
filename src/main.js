b0cd698792349bd0b0432f9337e5d76c5c4c5ccb
TzuChingg <suger5769@gmail.com>
2024-05-07 23:52:34 +0800

    feat: 🎸 VITE Basic settings

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import './assets/main.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueAxios, axios)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* global process */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_PATH,
   plugins: [
     vue(),
   ],
